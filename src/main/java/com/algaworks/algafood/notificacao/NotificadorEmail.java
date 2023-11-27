4a2f7c0d2278cc4201f5c931c53cc98b3afbae59
Clinton <clinton.almeida10@gmail.com>
2023-11-27 06:36:24 -0300

    Demais arquivos

---
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.modelo.Cliente;
+
+@Component
+public class NotificadorEmail implements Notificador {
+
+	public NotificadorEmail() {
+	
+		System.out.println("Notificador Email");
+	}
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+				cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+	
+}
---
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.service;
+
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.modelo.Cliente;
+import com.algaworks.algafood.notificacao.NotificadorEmail;
+
+@Component
+public class AtivacaoClienteService {
+	
+	private NotificadorEmail notificador;
+	
+	
+	
+	public AtivacaoClienteService(NotificadorEmail notificador) {
+		this.notificador = notificador;
+		System.out.println("AtivacaoClientService " + notificador  );
+	}
+
+
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		
+		notificador.notificar(cliente, "Seu cadastro no sistemas está ativo");
+	}
+}
