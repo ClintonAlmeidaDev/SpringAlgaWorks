8f8f057f1bae839ec7a2770ba03d29b84178ad5a
Clinton <clinton.almeida10@gmail.com>
2023-12-13 22:14:49 -0300

    Desafio lombok na classe restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,8 @@
 INSERT INTO COZINHA (nome) values('Tailandesa');
 INSERT INTO COZINHA (nome) values('Brasileira');
+INSERT INTO RESTAURANTE (nome, taxa_frete) values('Habibs', 10);
+INSERT INTO RESTAURANTE (nome, taxa_frete) values('Giraffas', 10);
+INSERT INTO RESTAURANTE (nome, taxa_frete) values('Grilleto', 10);
+INSERT INTO RESTAURANTE (nome, taxa_frete) values('Mcdonalds', 10);
+INSERT INTO RESTAURANTE (nome, taxa_frete) values('Bobs', 10);
+INSERT INTO RESTAURANTE (nome, taxa_frete) values('Divino', 10);
