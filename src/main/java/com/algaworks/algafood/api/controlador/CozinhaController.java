78f70d4ca4670ff5236e7630bfffa1f029057817
Clinton <clinton.almeida10@gmail.com>
2024-01-11 11:54:05 -0300

    Aula 4.27, implemtando o Service para o controller consumir o service e não o repository direto

--- src/main/java/com/algaworks/algafood/api/controlador/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controlador/CozinhaController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -29,6 +30,9 @@ public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
+	
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar(){
@@ -63,7 +67,7 @@ public class CozinhaController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-		return cozinhaRepository.salvar(cozinha);
+		return cadastroCozinha.salvar(cozinha);
 	}
 	
 	@PutMapping("/{cozinhaId}")
@@ -73,7 +77,7 @@ public class CozinhaController {
 		
 		if(cozinhaAtual != null) {
 		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-		cozinhaRepository.salvar(cozinhaAtual);
+		cadastroCozinha.salvar(cozinhaAtual);
 		
 		return ResponseEntity.ok(cozinhaAtual);
 		}
