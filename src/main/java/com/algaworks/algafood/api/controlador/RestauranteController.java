f05dcf4ad57ec5d5abc02bc05c0c16357c864f68
Clinton <clinton.almeida10@gmail.com>
2024-01-11 18:29:02 -0300

    Aula 4.30 conclusão do desafio de rest de restaurantes

--- src/main/java/com/algaworks/algafood/api/controlador/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controlador/RestauranteController.java
@@ -2,7 +2,7 @@ package com.algaworks.algafood.api.controlador;
 
 import java.util.List;
 
-
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,6 +57,29 @@ public class RestauranteController {
 		} catch(EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().build();
 		}
+		
+	}
+	
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante){
+		
+		
+		try {			
+			Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+			
+			if(restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+				
+				restauranteAtual = cadastroRestaurante.salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual);
+			}
+			
+			return ResponseEntity.notFound().build();
+		
+		}catch(EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
 		
 	}
 }
