dd01e7ab14ad294472721e9562098057e690cb56
Clinton <clinton.almeida10@gmail.com>
2024-01-27 15:39:24 -0300

    Aula 5.6, criando querys, usando no repositorio o findByNome para buscas já implantadas

--- src/main/java/com/algaworks/algafood/api/controlador/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controlador/TesteController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controlador;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,13 @@ public class TesteController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNOme(@RequestParam(value = "nome") String nome){
-//		return cozinhaRepository.consultarPorNome(nome);
-//	}
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNOme(String nome){
+		return cozinhaRepository.findTodasByNome(nome);
+	}
+	
+	@GetMapping("/cozinhas/unica-por-nome")
+	public Optional<Cozinha> cozinhasPorNomeUnico(String nome){
+		return cozinhaRepository.findByNome(nome);
+	}
 }
