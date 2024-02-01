83ac83e7041b931ee1281dd586f1a995f6da1987
Clinton <clinton.almeida10@gmail.com>
2024-02-01 20:21:02 -0300

    5.17. Implementando Specifications com SDJ

--- src/main/java/com/algaworks/algafood/api/controlador/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controlador/TesteController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.infrastructure.repository.spec.RestauranteComFreteGratisSpec;
+import com.algaworks.algafood.domain.infrastructure.repository.spec.RestauranteComNomeSemelhanteSpec;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
@@ -68,5 +70,14 @@ public class TesteController {
 	@GetMapping("/restaurantes/por-nome-e-frete")
 	public List<Restaurante> restaurantesPorNomeFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+	}
+	
+	@GetMapping("/restaurantes/com-frete-gratis")
+	public List<Restaurante> restauranteComFreteGratis(String nome){
+		
+		var comFreteGratis = new RestauranteComFreteGratisSpec();
+		var comNomeSemelhante = new RestauranteComNomeSemelhanteSpec(nome);
+		
+		return restauranteRepository.findAll(comFreteGratis.and(comNomeSemelhante));
 	}
 }
