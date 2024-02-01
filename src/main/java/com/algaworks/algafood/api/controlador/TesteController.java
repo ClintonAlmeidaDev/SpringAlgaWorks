4134b102b28e0abe5c8df44716b9520d7e1d2f7f
Clinton <clinton.almeida10@gmail.com>
2024-01-31 21:09:07 -0300

    Aula 5.11, aula de introdução a repositorio SDJ customizado

--- src/main/java/com/algaworks/algafood/api/controlador/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controlador/TesteController.java
@@ -64,4 +64,9 @@ public class TesteController {
 	public int cozinhaExists(Long cozinhaId){
 		return restauranteRepository.countByCozinhaId(cozinhaId);
 	}
+	
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantesPorNomeFrete(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
+	}
 }
