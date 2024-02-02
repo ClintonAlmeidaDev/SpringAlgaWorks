4a19ea4280e4f6ad5a138ad714ef9bc9eeab71b9
Clinton <clinton.almeida10@gmail.com>
2024-02-02 10:57:42 -0300

    5.19. Injetando o próprio repositório na implementação customizada e a anotação @Lazy

--- src/main/java/com/algaworks/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.domain.infrastructure.repository;
 
+import static com.algaworks.algafood.domain.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.algaworks.algafood.domain.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +15,20 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 
 @Repository
 public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	
+	@Autowired @Lazy
+	private RestauranteRepository restauranteRepository;
 	
 	@PersistenceContext
 	private EntityManager manager;
@@ -55,6 +63,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		
 		TypedQuery<Restaurante> query =  manager.createQuery(criteria);
 		return query.getResultList();
+	}
+
+	@Override
+	public List<Restaurante> findComFreteGratis(String nome) {
+		
+		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
 	}
 	
 }
